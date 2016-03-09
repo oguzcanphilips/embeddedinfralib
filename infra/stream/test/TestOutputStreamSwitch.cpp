4079c53ca8c70c4d8ef04e7e16cc12eb0069222f
Patrick van Kaam <patrick.van.kaam@philips.com>
2016-03-09 13:34:43 +0100

    Extract infra/stream/test/TestOutputStreamSwitch

---
+++ infra/stream/test/TestOutputStreamSwitch.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+#include "infra/stream/public/ByteOutputStream.hpp"
+#include "infra/stream/public/StdStringStream.hpp"
+#include "infra/stream/public/StringStream.hpp"
+#include <cstdint>
+
+TEST(OutputStreamSwitchTest, StreamToTextStream)
+{
+    infra::ByteOutputStream<uint8_t>::WithStorage<2> stream;
+
+    stream << infra::text << uint8_t(12);
+    EXPECT_EQ((std::array<uint8_t, 2>{{ '1', '2' }}), stream.Processed());
+}
+
+TEST(OutputStreamSwitchTest, TextStreamToStream)
+{
+    infra::StringOutputStream::WithStorage<2> stream;
+
+    stream << infra::data << uint8_t(012);
+    EXPECT_EQ("\012", stream.Storage());
+}
+
+TEST(OutputStreamSwitchTest, StreamToTextStreamToStream)
+{
+    infra::ByteOutputStream<uint8_t>::WithStorage<3> stream;
+
+    stream << infra::text << uint8_t(12) << infra::data << uint8_t(12);
+    EXPECT_EQ((std::array<uint8_t, 3>{{ '1', '2', 12 }}), stream.Processed());
+}
+
+TEST(OutputStreamSwitchTest, StreamFromBoundedString)
+{
+    infra::BoundedString::WithStorage<10> s = "abcd";
+    infra::StringOutputStream stream(s);
+
+    stream << uint8_t(12) << infra::data << uint8_t('a');
+
+    EXPECT_EQ("abcd12a", s);
+}
--- infra/stream/test/TestStringStream.cpp
+++ infra/stream/test/TestStringStream.cpp
@@ -5,40 +5,6 @@
 #include "infra/util/public/BoundedString.hpp"
 #include <cstdint>
 
-//TEST(StringStreamTest, StreamToTextStream)
-//{
-//    infra::ByteOutputStream<uint8_t>::WithStorage<2> stream;
-//
-//    stream << infra::text << uint8_t(12);
-//    EXPECT_EQ((std::array<uint8_t, 2>{{ '1', '2' }}), stream.Processed());
-//}
-//
-//TEST(StringStreamTest, TextStreamToStream)
-//{
-//    infra::StringOutputStream::WithStorage<2> stream;
-//
-//    stream << infra::data << uint8_t(012);
-//    EXPECT_EQ("\012", stream.Storage());
-//}
-//
-//TEST(StringStreamTest, StreamToTextStreamToStream)
-//{
-//    infra::ByteOutputStream<uint8_t>::WithStorage<3> stream;
-//
-//    stream << infra::text << uint8_t(12) << infra::data << uint8_t(12);
-//    EXPECT_EQ((std::array<uint8_t, 3>{{ '1', '2', 12 }}), stream.Processed());
-//}
-//
-//TEST(StringStreamTest, StreamFromBoundedString)
-//{
-//    infra::BoundedString::WithStorage<10> s = "abcd";
-//    infra::StringOutputStream stream(s);
-//
-//    stream << uint8_t(12) << infra::data << uint8_t('a');
-//
-//    EXPECT_EQ("abcd12a", s);
-//}
-
 TEST(StringStreamTest, ExtractDecimal)
 {
     infra::BoundedString::WithStorage<10> string("12");
