a2b766864c1117685e5de614cf79499015c7f31b
Richard Peters <richard@rpeters.org>
2016-07-03 14:06:57 +0200

    Add services/synchronous_util

---
+++ services/synchronous_util/public/SynchronousSpiMasterWithChipSelect.hpp
@@ -0,0 +1,23 @@
+#ifndef SYNCHRONOUS_SERVICES_SYNCHRONOUS_SPI_MASTER_WITH_CHIP_SELECT_HPP
+#define SYNCHRONOUS_SERVICES_SYNCHRONOUS_SPI_MASTER_WITH_CHIP_SELECT_HPP
+
+#include "hal/synchronous_interfaces/public/SynchronousSpi.hpp"
+#include "hal/synchronous_interfaces/public/SynchronousGpio.hpp"
+
+namespace services
+{
+    class SynchronousSpiMasterWithChipSelect
+        : public hal::SynchronousSpi
+    {
+    public:
+        SynchronousSpiMasterWithChipSelect(hal::SynchronousSpi& spi, hal::SynchronousOutputPin& chipSelect);
+
+        virtual void SendAndReceive(infra::ConstByteRange sendData, infra::ByteRange receiveData, Action nextAction) override;
+
+    private:
+        hal::SynchronousSpi& spi;
+        hal::SynchronousOutputPin& chipSelect;
+    };
+}
+
+#endif
