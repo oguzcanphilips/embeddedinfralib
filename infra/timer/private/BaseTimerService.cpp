e610ca6dbd0f12c614e3ae092c0ff3d65da922cb
Roy Creemers <roy.creemers@philips.com>
2016-11-14 15:06:33 +0100

    Fix endless loop when running hardware timer

--- infra/timer/private/BaseTimerService.cpp
+++ infra/timer/private/BaseTimerService.cpp
@@ -49,21 +49,19 @@ namespace infra
 
     void BaseTimerService::ProcessTicks()
     {
-    	bool continueLoop = notificationScheduled;
-        while (continueLoop)
-        {
-            TimeProgressed(ticksProgressed.exchange(0) * resolution);
+		TimeProgressed(ticksProgressed.exchange(0) * resolution);
 
-            previousTrigger = Now();
-            nextNotification = std::max(NextTrigger() - previousTrigger, Duration()) / resolution;
+		previousTrigger = Now();
+		nextNotification = std::max(NextTrigger() - previousTrigger, Duration()) / resolution;
 
-            // If in the meantime ticksProgressed has been increased beyond nextNotification,
-            // do not wait until the next timer tick, but immediately process this new amount
-            // Use the result of the assign to notificationScheduled, in order to avoid notificationScheduled
-            // being set to false, then receiving an interrupt setting it to true, and not handling the newly scheduled
-            // event immediately.
-            continueLoop = notificationScheduled = ticksProgressed >= nextNotification;
-        }
+		// If in the meantime ticksProgressed has been increased beyond nextNotification,
+		// the event has not been scheduled by the interrupt, so schedule the event here.
+		// Use the result of the assign to notificationScheduled, in order to avoid notificationScheduled
+		// being set to false, then receiving an interrupt setting it to true, and not handling the newly scheduled
+		// event immediately.
+		bool reschedule = notificationScheduled = ticksProgressed >= nextNotification;
+		if (reschedule)
+			infra::EventDispatcher::Instance().Schedule([this]() { ProcessTicks(); });
     }
 
     void BaseTimerService::EnterTestMode()
