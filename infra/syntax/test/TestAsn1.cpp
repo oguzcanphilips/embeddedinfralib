f4e242a8c131265773da8915138d16988e182940
Richard Peters <richard.peters@philips.com>
2017-04-20 07:43:49 +0200

    infra/syntax: Add Asn1

---
+++ infra/syntax/test/TestAsn1.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "infra/syntax/public/Asn1.hpp"
+
+TEST(Asn1ObjectFormatter, get_integer)
+{
+    std::array<uint8_t, 3> data = { { 0x02, 0x01, 0xab } };
+    infra::Asn1Sequence sequence(data);
+
+    EXPECT_EQ(infra::ConstByteRange(data.data() + 2, data.data() + 3), sequence.front().Integer());
+}
+
+TEST(Asn1ObjectFormatter, get_sequence)
+{
+    std::array<uint8_t, 8> data = { { 0x30, 0x06, 0x02, 0x04, 0xFF, 0xFF, 0xFF, 0xFF } };
+    infra::Asn1Sequence sequence(data);
+
+    EXPECT_EQ(infra::Asn1Sequence(infra::ConstByteRange(data.data() + 2, data.data() + 8)), sequence.begin()->Sequence());
+}
+
+TEST(Asn1ObjectFormatter, get_second_integer)
+{
+    std::array<uint8_t, 6> data = { { 0x02, 0x01, 0xab, 0x02, 0x01, 0xcd } };
+    infra::Asn1Sequence sequence(data);
+
+    EXPECT_EQ(infra::ConstByteRange(data.data() + 5, data.data() + 6), std::next(sequence.begin())->Integer());
+    EXPECT_EQ(infra::ConstByteRange(data.data() + 5, data.data() + 6), sequence[1].Integer());
+}
