9061cda8af9febd6f62791c8f8392c34b22b62ec
Oguzcan Oguz <oguz.oguzcan@philips.com>
2017-12-22 12:45:57 +0100

    Separate matchers in infra/util_test_helpers

--- infra/util/test_helper/BoundedStringMatcher.hpp
+++ infra/util/test_helper/BoundedStringMatcher.hpp
@@ -20,20 +20,6 @@ namespace testing
         Matcher(infra::BoundedStringBase<T> s) { *this = Eq(std::string(s.data(), s.size())); }
         Matcher(const char* s) { *this = Eq(std::string(s)); }
     };
-
-    template<class T>
-    class Matcher<infra::Optional<T>>
-        : public internal::MatcherBase<infra::Optional<T>>
-    {
-    public:
-        Matcher() = default;
-
-        explicit Matcher(const MatcherInterface<infra::Optional<T>>* impl)
-            : internal::MatcherBase<infra::Optional<T>>(impl)
-        {}
-
-        Matcher(infra::Optional<T> o) { *this = Eq(o); }
-    };
 }
 
 #endif
