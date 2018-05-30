6b8faf0bb7f8c1e76cf67561f20fa72e75bd30c8
Richard Peters <richard.peters@philips.com>
2018-05-30 14:29:20 +0200

    services/util/FlashRegion: Change asserts into really_asserts

--- services/util/FlashRegion.cpp
+++ services/util/FlashRegion.cpp
@@ -1,3 +1,4 @@
+#include "infra/util/ReallyAssert.hpp"
 #include "services/util/FlashRegion.hpp"
 
 namespace services
@@ -8,7 +9,7 @@ namespace services
         , numberOfSectors(numberOfSectors)
         , masterAddressOfStartSector(master.AddressOfSector(startSector))
     {
-        assert(startSector + numberOfSectors <= master.NumberOfSectors());
+        really_assert(startSector + numberOfSectors <= master.NumberOfSectors());
     }
 
     uint32_t FlashRegion::NumberOfSectors() const
@@ -33,19 +34,19 @@ namespace services
 
     void FlashRegion::WriteBuffer(infra::ConstByteRange buffer, uint32_t address, infra::Function<void()> onDone)
     {
-        assert(SectorOfAddress(address) <= numberOfSectors && SectorOfAddress(address + buffer.size()) <= numberOfSectors);
+        really_assert(SectorOfAddress(address) <= numberOfSectors && SectorOfAddress(address + buffer.size()) <= numberOfSectors);
         master.WriteBuffer(buffer, address + masterAddressOfStartSector, onDone);
     }
 
     void FlashRegion::ReadBuffer(infra::ByteRange buffer, uint32_t address, infra::Function<void()> onDone)
     {
-        assert(SectorOfAddress(address) <= numberOfSectors && SectorOfAddress(address + buffer.size()) <= numberOfSectors);
+        really_assert(SectorOfAddress(address) <= numberOfSectors && SectorOfAddress(address + buffer.size()) <= numberOfSectors);
         master.ReadBuffer(buffer, address + masterAddressOfStartSector, onDone);
     }
 
     void FlashRegion::EraseSectors(uint32_t beginIndex, uint32_t endIndex, infra::Function<void()> onDone)
     {
-        assert(beginIndex <= numberOfSectors && endIndex <= numberOfSectors);
+        really_assert(beginIndex <= numberOfSectors && endIndex <= numberOfSectors);
         master.EraseSectors(beginIndex + startSector, endIndex + startSector, onDone);
     }
 }
