14ff3d184f3851a726e1cf235c72e70853d51b2d
Richard Peters <richard.peters@philips.com>
2018-08-29 15:58:40 +0200

    infra/stream: Add LimitedOutputStream and StreamWriterMock

--- infra/stream/test/StreamMock.hpp
+++ infra/stream/test/StreamMock.hpp
@@ -3,6 +3,7 @@
 
 #include "gmock/gmock.h"
 #include "infra/stream/InputStream.hpp"
+#include "infra/stream/OutputStream.hpp"
 
 namespace infra
 {
@@ -17,6 +18,21 @@ namespace infra
         MOCK_METHOD1(ExtractContiguousRange, infra::ConstByteRange(std::size_t max));
         MOCK_CONST_METHOD0(Empty, bool());
         MOCK_CONST_METHOD0(Available, std::size_t());
+    };
+
+    class StreamWriterMock
+        : public infra::StreamWriter
+    {
+    public:
+        using infra::StreamWriter::StreamWriter;
+
+        MOCK_METHOD2(Insert, void(ConstByteRange range, StreamErrorPolicy& errorPolicy));
+        MOCK_CONST_METHOD0(Available, std::size_t());
+        MOCK_CONST_METHOD0(ConstructSaveMarker, const uint8_t*());
+        MOCK_CONST_METHOD1(GetProcessedBytesSince, std::size_t(const uint8_t* marker));
+        MOCK_METHOD1(SaveState, infra::ByteRange(const uint8_t* marker));
+        MOCK_METHOD1(RestoreState, void(infra::ByteRange range));
+        MOCK_METHOD1(Overwrite, infra::ByteRange(const uint8_t* marker));
     };
 }
 
